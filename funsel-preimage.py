3dcb2557699de84b78867de8271ec52a102cb976
Noel Maersk <veox+gits@veox.pw>
2018-04-28 21:40:42 +0300

    funsel-preimage: rework with `if __name__ == "__main__"`.

--- funsel-preimage.py
+++ funsel-preimage.py
@@ -7,47 +7,60 @@ from os.path import basename
 
 import web3
 
-argtypes = ['uint256', 'uint128', 'uint64', 'uint32', 'uint16', 'uint8',
+ARGTYPES = ['uint256', 'uint128', 'uint64', 'uint32', 'uint16', 'uint8',
             'int256', 'int128', 'int64', 'int32', 'int16', 'int8',
             'bytes32', 'bytes16','bytes8', 'bytes4', 'bytes1', 'bytes',
             'bool', 'string']
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--target', type=str, default='0x00000000',
-                    help='function selector to search for')
-parser.add_argument('-a', '--nargs', type=int, default=1,
-                    help='number of function arguments')
-parser.add_argument('-f', '--dictfile', type=str, default=None,
-                    help='file name to read dictionary of words from')
-parser.add_argument('-w', '--minwords', type=int, default=1,
-                    help='minimum number of words in function name')
-parser.add_argument('-W', '--maxwords', type=int, default=None,
-                    help='maximum number of words in function name')
-conf = parser.parse_args()
+def load_dictionary(filename: str) -> list:
+    with open(conf.dictfile) as fd:
+        words = fd.readlines()
+    words = [word.strip() for word in words]
 
-if not conf.maxwords: conf.maxwords = conf.minwords
+    return words
 
-filenameshort = basename(conf.dictfile)[:8]
-targetshort = conf.target[:-2]
+def run(conf):
+    # words' list for function name body
+    words = load_dictionary(conf.dictfile)
+    # used in print()
+    filenameshort = basename(conf.dictfile)[:8]
+    targetshort = conf.target[:-2]
 
-# words' list for function name body
-with open(conf.dictfile) as fd:
-    words = fd.readlines()
-words = [word.strip() for word in words]
+    for nwords in range(conf.minwords, conf.maxwords+1):
+        # iterate through possible argument combinations (same args are OK)
+        for argstuple in itertools.product(ARGTYPES, repeat=conf.nargs):
+            argstr = '(' + ','.join(argstuple) + ')'
 
-for nwords in range(conf.minwords, conf.maxwords+1):
-    for argstuple in itertools.product(argtypes, repeat=conf.nargs):
-        argstr = '(' + ','.join(argstuple) + ')'
+            print('#', filenameshort, nwords, 'words, args:', argstr)
 
-        print('>>>', filenameshort, nwords, 'words, args:', argstr)
+            # iterate through combos of words in function name (no repeats!)
+            for funwords in itertools.permutations(words, nwords):
+                # human-readable function signature
+                funsig = '_'.join(list(funwords)) + argstr
+                # 4-byte function selector
+                funsel = web3.Web3.toHex(web3.Web3.sha3(text=funsig)[:4])
 
-        for funwords in itertools.permutations(words, nwords):
-            # human-readable function signature
-            funsig = '_'.join(list(funwords)) + argstr
-            # 4-byte function selector
-            funsel = web3.Web3.toHex(web3.Web3.sha3(text=funsig)[:4])
+                # print close-enoughs
+                if funsel.startswith(targetshort): print(funsel, funsig)
+                # print match
+                if funsel == conf.target: print('### FOUND!', funsel, funsig)
 
-            # print close-enoughs
-            if funsel.startswith(targetshort): print(funsel, funsig)
-            # print match
-            if funsel == conf.target: print('>>>>>> FOUND!', funsel, funsig)
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', type=str, default='0x00000000',
+                        help='function selector to search for')
+    parser.add_argument('-a', '--nargs', type=int, default=1,
+                        help='number of function arguments')
+    parser.add_argument('-f', '--dictfile', type=str, default=None,
+                        help='file name to read dictionary of words from')
+    parser.add_argument('-w', '--minwords', type=int, default=1,
+                        help='minimum number of words in function name')
+    parser.add_argument('-W', '--maxwords', type=int, default=None,
+                        help='maximum number of words in function name')
+
+    conf = parser.parse_args()
+    if not conf.maxwords: conf.maxwords = conf.minwords
+
+    run(conf)
